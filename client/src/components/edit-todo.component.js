68d01894a9b92874acda9fd6c3b7bb37278fc114
Lavisha Bhambri <bhambrilavisha@gmail.com>
2021-06-29 19:33:24 +0530

    Adding all handlers

--- client/src/components/edit-todo.component.js
+++ client/src/components/edit-todo.component.js
@@ -46,6 +46,39 @@ export default class EditTodo extends Component{
         });
     }
 
+    onChangeTodoResponsible(e) {
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+
+    onChangeTodoPriority(e) {
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
+
+    onChangeTodoCompleted(e) {
+        this.setState({
+            todo_completed: !this.state.todo_completed  
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const obj = {
+            todo_description: this.state.todo_description,
+            todo_responsible: this.state.todo_responsible,
+            todo_priority: this.state.todo_priority,
+            todo_completed: this.state.todo_completed
+        };
+        axios.post('http://localhost:4000/todos/update/'+this.props.match.params.id, obj)
+            .then(res => console.log(res.data));
+
+        this.props.history.push('/');
+    }
+
+
     render() {
         return (
             <div>
